ead6cd40e42bc57e1b71c79c4935f20073b778bd
Constantine Iosifov <konstantin.iosifov@aventusit.eu>
2026-02-07 15:26:30 +0300

    lesson 2

---
+++ lesson 2/vars.py
@@ -0,0 +1,17 @@
+# num = 100
+# name = "Алексей"
+# first_number = 1
+# my_text = "text"
+# phone_number = 80292222222
+
+# name = "Constantine"
+# print(f"Hello, My name is {name}!")
+
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# print(f"Hello. My name is {name.capitalize()} {surname.capitalize()}.")
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(f"Result is: {a + b}")
